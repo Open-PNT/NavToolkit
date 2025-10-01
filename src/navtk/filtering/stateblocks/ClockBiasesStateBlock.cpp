690862282ff78e5a54c5d02879ebb11e019eaee5
Ilsoo Jeon <ilsoo.jeon@is4s.com>
2025-10-01 15:02:42 +0000

    Optimize ClockBiasesStateBlock

--- src/navtk/filtering/stateblocks/ClockBiasesStateBlock.cpp
+++ src/navtk/filtering/stateblocks/ClockBiasesStateBlock.cpp
@@ -38,19 +38,17 @@ DynamicsModel ClockBiasesStateBlock::generate_dynamics(GenXhatPFunction,
 		log_or_throw<std::invalid_argument>("dt should be >= 0 but it is {}", dt);
 	}
 
-	if (num_states == 2) {
-		Phi = Matrix{{1.0, dt}, {0.0, 1.0}};
-	} else {
-		Phi = Matrix{{1.0, dt, 0.5 * dt * dt}, {0, 1.0, dt}, {0, 0, 1.0}};
-	}
-
 	double dt2 = dt * dt;
 
-	// for both calc_Hwang_Q_no_flicker and original calc_Hwang_Brown_Q
-	double ct = pi_sq * model.h_m2 * dt2;
+	double rand_walk   = pi_sq * model.h_m2;
+	double rand_walk2  = rand_walk * 2;
+	double rand_walk23 = rand_walk * (2.0 / 3);
 
-	// for calc_Hwang_Q
-	double ct1 = model.h_m1 * dt + pi_sq * model.h_m2 * dt2;
+	double white_noise = 0.5 * model.h_0;
+
+	double m00 = (white_noise * dt) + (rand_walk23 * dt * dt2);
+	double m11 = rand_walk2 * dt;
+	double ct  = pi_sq * model.h_m2 * dt2;
 
 	Matrix Qd;
 
@@ -58,42 +56,24 @@ DynamicsModel ClockBiasesStateBlock::generate_dynamics(GenXhatPFunction,
 	switch (choice) {
 	case ClockChoice::QD:
 		// defining basic expanded matrix
-		Qd = Matrix{
-		    {model.h_0 / 2 * dt + 2 * model.h_m1 * dt2 + 2.0 / 3 * pi_sq * model.h_m2 * dt2 * dt,
-		     ct,
-		     0},
-		    {ct, 4 * model.h_m1 + 2 * pi_sq * model.h_m2 * dt, 0},
-		    {0, 0, 0}};
-
-
+		Qd = {{m00 + (2 * model.h_m1 * dt2), ct, 0}, {ct, (4 * model.h_m1) + m11, 0}, {0, 0, 0}};
 		break;
 
 	case ClockChoice::QD1:
-
-		Qd = Matrix{{model.h_0 / 2 * dt + 2.0 / 3 * pi_sq * model.h_m2 * dt2 * dt, ct, 0},
-		            {ct, model.h_0 / (2 * dt) + (8.0 / 3) * pi_sq * model.h_m2 * dt, 0},
-		            {0, 0, 0}};
-
+		Qd = {{m00, ct, 0}, {ct, (white_noise / dt) + ((4.0 / 3) * m11), 0}, {0, 0, 0}};
 		break;
 
-	case ClockChoice::QD2:
-
-		Qd = Matrix{{0.5 * model.h_0 * dt + (2.0 / 3) * pi_sq * model.h_m2 * dt * dt2, ct1, 0},
-		            {ct1, 2 * pi_sq * model.h_m2 * dt, 0},
-		            {0, 0, 0}};
-
-		break;
+	case ClockChoice::QD2: {
+		double ct1 = ct + model.h_m1 * dt;
+		Qd         = {{m00, ct1, 0}, {ct1, m11, 0}, {0, 0, 0}};
+	} break;
 
 	case ClockChoice::QD3:
-
-		Qd = Matrix{{0.5 * model.h_0 * dt + (2.0 / 3) * pi_sq * model.h_m2 * dt * dt2, ct, 0},
-		            {ct, 2 * pi_sq * model.h_m2 * dt, 0},
-		            {0, 0, 0}};
+		Qd = {{m00, ct, 0}, {ct, m11, 0}, {0, 0, 0}};
+		break;
 	}
 
-	auto g = [Phi = Phi](Vector x) { return dot(Phi, x); };
 	if (num_states == 3) {
-
 
 		Matrix q_three{
 		    {(1.0 / 20) * model.q3 * dt2 * dt2 * dt,
@@ -104,9 +84,15 @@ DynamicsModel ClockBiasesStateBlock::generate_dynamics(GenXhatPFunction,
 		     (1.0 / 2) * model.q3 * dt2},
 		    {(1.0 / 6) * model.q3 * dt2 * dt, (1.0 / 2) * model.q3 * dt2, model.q3 * dt}};
 
+		Phi    = Matrix{{1.0, dt, 0.5 * dt2}, {0, 1.0, dt}, {0, 0, 1.0}};
+		auto g = [Phi = Phi](Vector x) { return dot(Phi, x); };
+
 		return DynamicsModel(g, Phi, Qd + q_three);
 
 	} else {
+
+		Phi    = Matrix{{1.0, dt}, {0.0, 1.0}};
+		auto g = [Phi = Phi](Vector x) { return dot(Phi, x); };
 
 		return DynamicsModel(g, Phi, xt::view(Qd, xt::range(0, 2), xt::range(0, 2)));
 	}
